5b0017b9891945e1e07c7fb01e815e359c88fca4
UK992 <urbankrajnc92@gmail.com>
2013-12-13 18:44:13 +0100

    Add support for Firefox 27

--- content/editor.js
+++ content/editor.js
@@ -9,7 +9,11 @@
 let global = this;
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import("resource://gre/modules/Services.jsm");
-Cu.import("resource:///modules/source-editor.jsm");
+try {
+  Cu.import("resource:///modules/source-editor.jsm");
+} catch (ex) {
+  Cu.import("resource:///modules/devtools/sourceeditor/source-editor.jsm");
+}
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/NetUtil.jsm");
 Cu.import("resource://gre/modules/FileUtils.jsm");
